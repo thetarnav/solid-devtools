74625baefcaa38c1232568d941e0082e112fdacf
thetarnav <gthetarnav@gmail.com>
2022-05-15 22:42:56 +0200

    improve styles of rerun indication

--- packages/ui/src/ownerNode.tsx
+++ packages/ui/src/ownerNode.tsx
@@ -1,9 +1,20 @@
 import { ReactiveGraphOwner, OwnerType } from "@shared/graph"
 import { For, JSX, Show } from "solid-js"
 import { DeepReadonly } from "solid-js/store"
-import { tw } from "./twind"
+import { colors, tw } from "./twind"
 import { TransitionGroup, animateExit, animateEnter } from "@otonashixav/solid-flip"
 // import {TransitionGroup} from "solid-transition-group"
+
+function hexToRgb(hex: string, alpha?: number) {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+	if (!result) return null
+	const r = parseInt(result[1], 16),
+		g = parseInt(result[2], 16),
+		b = parseInt(result[3], 16)
+	return alpha === undefined ? `rgb(${r},${g},${b})` : `rgba(${r},${g},${b}, ${alpha})`
+}
+
+const highlightRgba = hexToRgb(colors.cyan[400], 0.6)
 
 export function OwnerChildren(props: { children: DeepReadonly<ReactiveGraphOwner[]> }) {
 	return (
@@ -23,18 +34,23 @@ export function OwnerNode(props: { owner: DeepReadonly<ReactiveGraphOwner> }): J
 	return (
 		<div
 			ref={ref}
-			class={tw`bg-cyan-200 bg-opacity-5 border-0 border-t-[1px] border-l-[1px] border-cyan-900 border-opacity-30 outline-[1px] pt-1 pl-0.5`}
+			class={tw`bg-cyan-200 bg-opacity-5 border-0 border-t-[1px] border-l-[1px] border-cyan-900 border-opacity-30 outline-[1px] pt-1 pl-1`}
 		>
-			<div
-				class={tw`pb-1 pr-2`}
-				style={{
-					// TODO: better styles for computation rerun
-					"background-color": props.owner.rerun ? "red" : null,
-				}}
-			>
-				<p class={tw`italic pb-0.5`}>
-					{type === OwnerType.Component ? `<${name}>` : name}{" "}
-					<span class={tw`text-[10px] opacity-40`}>{typeName}</span>
+			<div class={tw`pr-2`}>
+				<p>
+					<span
+						class={tw`
+							pl-1 pr-1 pb-0.5 pt-0.5
+							rounded italic
+							transition-color
+						`}
+						style={{
+							"background-color": props.owner.rerun ? highlightRgba : null,
+						}}
+					>
+						{type === OwnerType.Component ? `<${name}>` : name}{" "}
+						<span class={tw`text-[10px] opacity-40`}>{typeName}</span>
+					</span>
 				</p>
 				{/* <ValueNode value={value} />
 				<div class="flex space-x-1">
@@ -50,7 +66,12 @@ export function OwnerNode(props: { owner: DeepReadonly<ReactiveGraphOwner> }): J
 				</div>
 			</Show> */}
 			{/* <Show when={children().length}> */}
-			<div class={tw`pl-4 pt-1`}>
+			<div
+				class={tw`pl-4 pt-1 transition-opacity duration-500`}
+				style={{
+					opacity: props.owner.rerun ? 0.3 : 1,
+				}}
+			>
 				<OwnerChildren children={children()} />
 			</div>
 			{/* </Show> */}
--- packages/ui/src/twind.ts
+++ packages/ui/src/twind.ts
@@ -1,7 +1,9 @@
 import { create, cssomSheet, ThemeConfiguration } from "twind"
-import * as colors from "twind/colors"
+import * as _colors from "twind/colors"
 
 export const sheet = cssomSheet({ target: new CSSStyleSheet() })
+
+export const colors = _colors
 
 export const theme: ThemeConfiguration = {
 	colors,
