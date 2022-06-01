5a8fb22b6dc1d1e06cfd98e0078e9ae654ea0621
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-01 18:14:03 +0200

    attach shadow dom root to the page

--- packages/locator/src/index.tsx
+++ packages/locator/src/index.tsx
@@ -1,11 +1,16 @@
+import { Accessor, Component, createEffect, createSignal, onCleanup } from "solid-js"
+import { Portal, render } from "solid-js/web"
+import { isProd } from "@solid-primitives/utils"
 import { MappedComponent } from "@shared/graph"
-import { Accessor, createEffect, createSignal } from "solid-js"
+import { sheet } from "@ui"
 
 export function useLocator({ components }: { components: Accessor<MappedComponent[]> }): {
 	enabled: Accessor<boolean>
 } {
 	// TODO: enable only when the page window is opened
 	const [enabled, setEnabled] = createSignal(true)
+
+	attachLocator()
 
 	createEffect(() => {
 		for (const comp of components()) {
@@ -14,4 +19,19 @@ export function useLocator({ components }: { components: Accessor<MappedComponen
 	})
 
 	return { enabled }
+}
+
+function attachLocator() {
+	if (isProd) return
+	const root = (<div class="solid-devtools-locator"></div>) as HTMLDivElement
+	const shadow = root.attachShadow({ mode: "open" })
+	const inShadowRoot = document.createElement("div")
+	shadow.appendChild(inShadowRoot)
+	shadow.adoptedStyleSheets = [sheet.target]
+	onCleanup(render(() => root, document.body))
+	onCleanup(render(() => <Locator />, inShadowRoot))
+}
+
+const Locator: Component = props => {
+	return <div>Hello :)</div>
 }
