75b005147e853c84b38ebcbdc85b82514b40432b
thetarnav <thetarnav@users.noreply.github.com>
2022-06-12 20:47:22 +0000

    Format

--- playgrounds/start/src/entry-client.tsx
+++ playgrounds/start/src/entry-client.tsx
@@ -1,4 +1,4 @@
-import { hydrate } from "solid-js/web";
-import { StartClient } from "solid-start/entry-client";
+import { hydrate } from "solid-js/web"
+import { StartClient } from "solid-start/entry-client"
 
-hydrate(() => <StartClient />, document);
+hydrate(() => <StartClient />, document)
--- playgrounds/start/src/lib/api.ts
+++ playgrounds/start/src/lib/api.ts
@@ -1,18 +1,17 @@
-import { isServer } from "solid-js/web";
+import { isServer } from "solid-js/web"
 
-const story = (path: string) => `https://node-hnapi.herokuapp.com/${path}`;
-const user = (path: string) =>
-  `https://hacker-news.firebaseio.com/v0/${path}.json`;
+const story = (path: string) => `https://node-hnapi.herokuapp.com/${path}`
+const user = (path: string) => `https://hacker-news.firebaseio.com/v0/${path}.json`
 
 export default async function fetchAPI(path: string) {
-  const url = path.startsWith("user") ? user(path) : story(path);
-  const headers = isServer ? { "User-Agent": "chrome" } : {};
+	const url = path.startsWith("user") ? user(path) : story(path)
+	const headers = isServer ? { "User-Agent": "chrome" } : {}
 
-  let response = await fetch(url, { headers });
-  let json = await response.json();
+	let response = await fetch(url, { headers })
+	let json = await response.json()
 
-  if (json === null) {
-    return { error: "Not found" };
-  }
-  return json;
+	if (json === null) {
+		return { error: "Not found" }
+	}
+	return json
 }
