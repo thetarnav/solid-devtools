17e83adb80c2c65425b0f36b4382bb9ccf0a39ba
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-15 20:45:33 +0200

    remove dev script

--- env.d.ts
+++ env.d.ts
@@ -8,3 +8,12 @@ declare global {
 		adoptedStyleSheets: CSSStyleSheet[]
 	}
 }
+
+declare global {
+	interface Array<T> {
+		includes(searchElement: unknown, fromIndex?: number): boolean
+	}
+	interface ReadonlyArray<T> {
+		includes(searchElement: unknown, fromIndex?: number): boolean
+	}
+}
