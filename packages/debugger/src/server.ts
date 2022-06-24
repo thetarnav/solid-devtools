3f723fde33d8fa91792fe8ed97e45fc41f2ef5af
Damian Tarnawski <gthetarnav@gmail.com>
2022-06-24 14:41:41 +0200

    correct server exports

--- packages/debugger/src/server.ts
+++ packages/debugger/src/server.ts
@@ -5,4 +5,4 @@ export const Debugger: typeof API.Debugger = props => props.children
 
 export const attachDebugger: typeof API.attachDebugger = noop
 
-export const useLocatorPlugin: typeof API.useLocatorPlugin = noop
+export const registerDebuggerPlugin: typeof API.registerDebuggerPlugin = noop
