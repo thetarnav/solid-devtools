f5b3ed2cc748feb82aeecf842549d9cd168a6d56
Damian Tarnawski <gthetarnav@gmail.com>
2022-08-03 21:57:49 +0200

    correct esbuild solidPlugin type issues

--- configs/tsup.config.ts
+++ configs/tsup.config.ts
@@ -2,21 +2,22 @@ import { defineConfig } from "tsup"
 import { solidPlugin } from "esbuild-plugin-solid"
 
 export default (
-	extension: "tsx" | "ts" = "ts",
-	target: ("cjs" | "esm")[] | "cjs" | "esm" = ["cjs", "esm"],
-	server: boolean = false,
-	additionalEntries: string[] = [],
+  extension: "tsx" | "ts" = "ts",
+  target: ("cjs" | "esm")[] | "cjs" | "esm" = ["cjs", "esm"],
+  server: boolean = false,
+  additionalEntries: string[] = [],
 ) => {
-	const entry = `src/index.${extension}`
-	const baseEntries = server ? [entry, `src/server.${extension}`] : [entry]
-	const mappedAdditionalEntries = additionalEntries.map(entry => `src/${entry}.${extension}`)
-	return defineConfig({
-		clean: true,
-		dts: {
-			entry: [entry, ...mappedAdditionalEntries],
-		},
-		format: Array.isArray(target) ? target : [target],
-		entryPoints: [...baseEntries, ...mappedAdditionalEntries],
-		esbuildPlugins: extension === "tsx" ? [solidPlugin()] : undefined,
-	})
+  const entry = `src/index.${extension}`
+  const baseEntries = server ? [entry, `src/server.${extension}`] : [entry]
+  const mappedAdditionalEntries = additionalEntries.map(entry => `src/${entry}.${extension}`)
+  return defineConfig({
+    clean: true,
+    dts: {
+      entry: [entry, ...mappedAdditionalEntries],
+    },
+    format: Array.isArray(target) ? target : [target],
+    entryPoints: [...baseEntries, ...mappedAdditionalEntries],
+    // solidPlugin() returns an incompatible plugin type from 0.14.53 esbuild version
+    esbuildPlugins: extension === "tsx" ? [solidPlugin() as any] : undefined,
+  })
 }
