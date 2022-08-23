254cf8dd56d941cae3355052652edbaee509cac1
Damian Tarnawski <gthetarnav@gmail.com>
2022-08-23 13:18:38 +0200

    improve manifest

--- packages/extension/manifest.ts
+++ packages/extension/manifest.ts
@@ -1,12 +1,22 @@
 import { defineManifest } from "@crxjs/vite-plugin"
 import { version } from "./package.json"
 
-export default defineManifest({
+// Convert from Semver (example: 0.1.0-beta6)
+const [major, minor, patch, label = "0"] = version
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, "")
+  // split into version parts
+  .split(/[.-]/)
+
+export default defineManifest(env => ({
   manifest_version: 3,
-  name: "Solid Devtools",
+  name: `${env.mode === "production" ? "" : "[DEV] "}Solid Devtools`,
   description: "Chrome Developer Tools extension for debugging SolidJS applications.",
   homepage_url: "https://github.com/thetarnav/solid-devtools",
-  version,
+  // up to four numbers separated by dots
+  version: `${major}.${minor}.${patch}.${label}`,
+  // semver is OK in "version_name"
+  version_name: version,
   author: "Damian Tarnawski",
   minimum_chrome_version: "94",
   devtools_page: "devtools/devtools.html",
@@ -37,4 +47,4 @@ export default defineManifest({
     "48": "assets/icons/solid-normal-48.png",
     "128": "assets/icons/solid-normal-128.png",
   },
-})
+}))
