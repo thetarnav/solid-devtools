08441d2363639c2e2d607a2786d038cbbb34bc67
Damian Tarnawski <gthetarnav@gmail.com>
2022-08-25 19:41:59 +0200

    fix test

--- packages/locator/src/server.ts
+++ packages/locator/src/server.ts
@@ -4,4 +4,5 @@ import * as API from "."
 export const useLocator: typeof API.useLocator = noop
 export const registerLocatorPlugin: typeof API.registerLocatorPlugin = () => ({
   selected: () => null,
+  setTargetElement: () => null as any,
 })
