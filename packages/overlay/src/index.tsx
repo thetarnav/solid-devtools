33c0f73dd868d3956b2487343d834cb8bdb7bda2
Damian Tarnawski <gthetarnav@gmail.com>
2022-10-11 23:36:17 +0200

    Add a way to resize the overlay

--- packages/overlay/src/index.tsx
+++ packages/overlay/src/index.tsx
@@ -1,5 +1,8 @@
-import { Component, onCleanup, Show } from 'solid-js'
+import { Component, createSignal, onCleanup, Show } from 'solid-js'
 import { Dynamic, isServer, Portal } from 'solid-js/web'
+import { makeEventListener } from '@solid-primitives/event-listener'
+import { clamp } from '@solid-primitives/utils'
+import { createBodyCursor } from '@solid-primitives/cursor'
 import { Devtools, Icon } from '@solid-devtools/frontend'
 import { createInternalRoot } from '@solid-devtools/debugger'
 import { createController, enabled, setEnabled } from './controller'
@@ -27,16 +30,39 @@ export const DevtoolsOverlay: Component = props => {
 }
 
 const Overlay: Component = props => {
+  const [progress, setProgress] = createSignal(0.5)
+  const [dragging, setDragging] = createSignal(false)
+
+  makeEventListener(window, 'pointermove', e => {
+    if (!dragging()) return
+    const vh = window.innerHeight
+    setProgress(clamp(e.y, 200, vh - 200) / vh)
+  })
+  makeEventListener(window, 'pointerup', setDragging.bind(void 0, false))
+
+  createBodyCursor(() => dragging() && 'row-resize')
+
   return (
     <Portal useShadow mount={document.documentElement}>
-      <div class="overlay__container" data-open={enabled()}>
+      <div
+        class="overlay__container"
+        data-open={enabled()}
+        style={{ '--progress': progress() * 100 + '%' }}
+      >
         <button class="overlay__toggle-button" onClick={() => setEnabled(p => !p)}>
-          Devtools{' '}
+          Devtools
           <Dynamic
             component={enabled() ? Icon.EyeSlash : Icon.Eye}
             class="overlay__toggle-button__icon"
           />
         </button>
+        <div
+          class="overlay__container__resizer"
+          onPointerDown={e => {
+            e.preventDefault()
+            setDragging(true)
+          }}
+        />
         <div class="overlay__container__inner">
           <Show when={enabled()}>
             {() => {
