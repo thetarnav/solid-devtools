36719f231916ba565022ee201ad29421fbafe656
Damian Tarnawski <gthetarnav@gmail.com>
2022-10-11 22:28:31 +0200

    Move overlay styles to a css file

--- packages/overlay/src/Overlay.tsx
+++ packages/overlay/src/Overlay.tsx
@@ -5,6 +5,7 @@ import { createInternalRoot } from '@solid-devtools/debugger'
 import { createController, enabled, setEnabled } from './controller'
 
 import frontendStyles from '@solid-devtools/frontend/dist/index.css'
+import overlayStyles from './styles.css'
 
 export const DevtoolsOverlay: Component = props => {
   if (isServer) return ''
@@ -44,49 +45,7 @@ const Overlay: Component = props => {
         </Show>
       </div>
       <style>{frontendStyles}</style>
-      <style>
-        {
-          /* css */ `
-          .overlay__container {
-            position: fixed;
-            inset: 100% 0 0 0;
-            z-index: 999999;
-          }
-          .overlay__container[data-open='true'] {
-            inset: 50% 0 0 0;
-          }
-
-          .overlay__toggle-button {
-            position: absolute;
-            bottom: 100%;
-            right: 0;
-            margin: 0.5rem;
-            height: 1.2rem;
-            padding: 0 0.3rem;
-            font-size: 0.7rem;
-            background: rgb(6 33 68 / 0.9);
-            border: 1px solid #89b6ff;
-            color: #89b6ff;
-            border-radius: 0.4rem;
-            cursor: pointer;
-            display: flex;
-            align-items: center;
-            column-gap: 0.2rem;
-          }
-          .overlay__toggle-button:hover {
-            background: rgb(12 55 113 / 0.9);
-          }
-          .overlay__toggle-button:active {
-            background: rgb(10 47 96 / 0.9);
-          }
-
-          .overlay__toggle-button__icon {
-            width: 0.9rem;
-            height: 0.9rem;
-          }
-        `
-        }
-      </style>
+      <style>{overlayStyles}</style>
     </Portal>
   )
 }
