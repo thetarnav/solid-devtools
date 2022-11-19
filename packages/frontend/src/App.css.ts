2b0e05321a54579b296fb931f067d962cf635fd9
Damian Tarnawski <gthetarnav@gmail.com>
2022-11-19 19:39:47 +0100

    Add and style search component

--- packages/frontend/src/App.css.ts
+++ packages/frontend/src/App.css.ts
@@ -1,5 +1,15 @@
 import { style } from '@vanilla-extract/css'
-import { dark, spacing, color, rounded, theme, media } from '@/ui/theme'
+import {
+  spacing,
+  color,
+  rounded,
+  theme,
+  border,
+  padding,
+  insetY,
+  centerChild,
+  transition,
+} from '@/ui/theme'
 import { toggleButton } from '@/ui/components/button/button.css'
 import { vars, panelBg, panelBorder, defaultTextColor, colorDisabled } from './ui/theme/vars.css'
 
@@ -25,12 +35,7 @@ export const header = style({
   columnGap: spacing[2],
   backgroundColor: panelBg,
   borderBottom: panelBorder,
-  color: color.black,
-  ...media({
-    [dark]: {
-      color: color.gray[50],
-    },
-  }),
+  color: defaultTextColor,
 })
 
 export const subtitle = style({
@@ -40,51 +45,132 @@ export const subtitle = style({
   marginTop: spacing[1],
 })
 
-export const select = style({
+export const locatorButton = style({
   width: spacing[7],
   height: spacing[7],
 })
-export const selectIcon = style({
+export const locatorIcon = style({
   width: spacing[4.5],
   height: spacing[4.5],
 })
 
-export const options = style({
-  position: 'relative',
-  marginLeft: 'auto',
-})
-export const optionsButton = style([
-  toggleButton,
-  {
-    marginLeft: 'auto',
-    width: spacing[7],
-    height: spacing[7],
-  },
-])
-export const optionsIcon = style({
-  width: spacing[4.5],
-  height: spacing[4.5],
-})
+export const search = (() => {
+  const height = spacing[7]
 
-export const optionsPanel = style({
-  position: 'absolute',
-  zIndex: 9999,
-  width: 'max-content',
-  top: '0',
-  right: '100%',
-  marginRight: spacing[2],
-  padding: spacing[2],
-  ...rounded('md'),
-  backgroundColor: color.gray[100],
-  border: `1px solid ${color.gray[200]}`,
-})
+  const form = style({
+    position: 'relative',
+    height,
+    overflow: 'hidden',
+  })
 
-export const optionsMenu = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'stretch',
-  gap: spacing[2],
-})
+  const input = style({
+    height,
+    boxSizing: 'border-box',
+    width: spacing[48],
+    outline: 'unset',
+    background: 'unset',
+    color: 'inherit',
+    fontFamily: 'inherit',
+    ...border(color.gray[700]),
+    ...rounded(),
+    ...padding(0, 6),
+    ...transition('padding'),
+    lineHeight: spacing[8],
+    '::placeholder': {
+      color: colorDisabled,
+    },
+    ':focus': {
+      ...border(color.gray[500]),
+    },
+    selectors: {
+      [`${form}:focus-within &`]: {
+        paddingLeft: spacing[2],
+      },
+    },
+  })
+
+  const edgeContainerBase = style({
+    position: 'absolute',
+    height: `calc(100% - ${spacing[2]})`,
+    ...insetY(1),
+    ...centerChild,
+  })
+  const iconBase = style({
+    width: spacing[4],
+    height: spacing[4],
+    color: colorDisabled,
+  })
+
+  return {
+    form,
+    input,
+    iconContainer: style([
+      edgeContainerBase,
+      {
+        left: 0,
+        paddingLeft: spacing[1.5],
+        ...transition('transform'),
+        selectors: {
+          [`${form}:focus-within &`]: {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+    ]),
+    icon: iconBase,
+    clearButton: style([
+      edgeContainerBase,
+      {
+        right: spacing[1],
+        ...padding(0, 0.5),
+        ...rounded(),
+        ':hover': {
+          backgroundColor: color.gray[700],
+        },
+      },
+    ]),
+    clearIcon: iconBase,
+  }
+})()
+
+export const options = (() => {
+  return {
+    container: style({
+      position: 'relative',
+      marginLeft: 'auto',
+    }),
+    button: style([
+      toggleButton,
+      {
+        marginLeft: 'auto',
+        width: spacing[7],
+        height: spacing[7],
+      },
+    ]),
+    icon: style({
+      width: spacing[4.5],
+      height: spacing[4.5],
+    }),
+    panel: style({
+      position: 'absolute',
+      zIndex: 9999,
+      width: 'max-content',
+      top: '0',
+      right: '100%',
+      marginRight: spacing[2],
+      padding: spacing[2],
+      ...rounded('md'),
+      backgroundColor: color.gray[100],
+      border: `1px solid ${color.gray[200]}`,
+    }),
+    menu: style({
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'stretch',
+      gap: spacing[2],
+    }),
+  }
+})()
 
 export const content = style({
   overflow: 'hidden',
--- packages/frontend/src/ui/icons/index.tsx
+++ packages/frontend/src/ui/icons/index.tsx
@@ -501,3 +501,26 @@ export const Code: IconComponent = props => (
     </g>
   </svg>
 )
+
+export const Search: IconComponent = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class={props.class}>
+    <circle
+      cx="116"
+      cy="116"
+      r="84"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="16"
+    />
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="16"
+      d="M175.4 175.4L224 224"
+    />
+  </svg>
+)
