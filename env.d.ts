27cd002271432a30a9d80f52c4aec24235f2e918
Damian Tarnawski <gthetarnav@gmail.com>
2023-01-09 18:36:42 +0100

    Add eslint comments

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,11 @@
 export {}
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Array<T> {
     includes(searchElement: unknown, fromIndex?: number): boolean
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ReadonlyArray<T> {
     includes(searchElement: unknown, fromIndex?: number): boolean
   }
