c94d5b093d7a8d26f59a985a98271a09d72277c4
Damian Tarnawski <gthetarnav@gmail.com>
2023-02-16 22:10:10 +0100

    Test adding names to effects

--- packages/main/src/vite/test/name.test.ts
+++ packages/main/src/vite/test/name.test.ts
@@ -1,139 +1,245 @@
 import { describe, test } from 'vitest'
 import plugin from '../name'
-import { assertTransform } from './setup'
+import { assertTransform, testTransform } from './setup'
 
-// Positive tests
-for (const [create, module, addExtraArg] of [
-  ['createSignal', 'solid-js'],
-  ['createMemo', 'solid-js', '1'],
-  ['createStore', 'solid-js/store'],
-  ['createMutable', 'solid-js/store'],
-] as const) {
-  const extraArg = addExtraArg ? 'undefined, ' : ''
-  describe(create, () => {
-    for (const [type, importStatement, creator] of [
-      ['named import', `import { ${create} } from "${module}";`, create],
-      ['renamed import', `import { ${create} as foo } from "${module}";`, 'foo'],
-      ['namespace import', `import * as foo from "${module}";`, `foo.${create}`],
-    ] as const) {
-      describe(type, () => {
-        test('no default value', () => {
-          const src = `${importStatement}
-  const signal = ${creator}();`
+describe('returning primitives', () => {
+  // Positive tests
+  for (const [create, module, addExtraArg] of [
+    ['createSignal', 'solid-js'],
+    ['createMemo', 'solid-js', '1'],
+    ['createStore', 'solid-js/store'],
+    ['createMutable', 'solid-js/store'],
+  ] as const) {
+    const extraArg = addExtraArg ? 'undefined, ' : ''
+    describe(create, () => {
+      for (const [type, importStatement, creator] of [
+        ['named import', `import { ${create} } from "${module}";`, create],
+        ['renamed import', `import { ${create} as foo } from "${module}";`, 'foo'],
+        ['namespace import', `import * as foo from "${module}";`, `foo.${create}`],
+      ] as const) {
+        describe(type, () => {
+          testTransform(
+            'no default value',
+            `${importStatement}
+    const signal = ${creator}();`,
+            `${importStatement}
+  const signal = ${creator}(undefined, ${extraArg}{
+    name: "signal"
+  });`,
+            plugin,
+            true,
+          )
 
-          const expectedOutput = `${importStatement}
-const signal = ${creator}(undefined, ${extraArg}{
-  name: "signal"
-});`
+          testTransform(
+            'default value',
+            `${importStatement}
+  const signal = ${creator}(5);`,
+            `${importStatement}
+  const signal = ${creator}(5, ${extraArg}{
+    name: "signal"
+  });`,
+            plugin,
+            true,
+          )
 
-          assertTransform(src, expectedOutput, plugin)
+          testTransform(
+            'empty options',
+            `${importStatement}
+          const rest = {};
+          const signal = ${creator}(5, ${extraArg}{});`,
+
+            `${importStatement}
+          const rest = {};
+          const signal = ${creator}(5, ${extraArg}{
+            name: "signal"
+          });`,
+
+            plugin,
+            true,
+          )
+
+          testTransform(
+            'options excluding name',
+            `${importStatement}
+          const rest = {};
+          const signal = ${creator}(5, ${extraArg}{ equals: false, ...rest });`,
+
+            `${importStatement}
+  const rest = {};
+  const signal = ${creator}(5, ${extraArg}{
+    name: "signal",
+    equals: false,
+    ...rest
+  });`,
+
+            plugin,
+            true,
+          )
+
+          testTransform(
+            'options including name',
+            `${importStatement}
+  const rest = {};
+  const signal = ${creator}(5, ${extraArg}{ equals: false, name: "foo", ...rest });`,
+
+            `${importStatement}
+  const rest = {};
+  const signal = ${creator}(5, ${extraArg}{
+    equals: false,
+    name: "foo",
+    ...rest
+  });`,
+
+            plugin,
+            true,
+          )
+
+          testTransform(
+            'array of length 1',
+            `${importStatement}
+  const [signal] = ${creator}(5);`,
+
+            `${importStatement}
+  const [signal] = ${creator}(5, ${extraArg}{
+    name: "signal"
+  });`,
+
+            plugin,
+            true,
+          )
+
+          testTransform(
+            'array of length 2',
+            `${importStatement}
+  const [signal, setSignal] = ${creator}(5);`,
+
+            `${importStatement}
+  const [signal, setSignal] = ${creator}(5, ${extraArg}{
+    name: "signal"
+  });`,
+
+            plugin,
+            true,
+          )
         })
+      }
+    })
+  }
 
-        test('default value', () => {
-          const src = `${importStatement}
-const signal = ${creator}(5);`
+  // Negative tests
+  for (const [create, module] of [
+    ['createSignal', 'solid-js/store'],
+    ['createMemo', 'solid-js/store'],
+    ['createStore', 'solid-js'],
+    ['createMutable', 'solid-js'],
+  ] as const) {
+    describe(create, () => {
+      test(`no import`, () => {
+        const src = `const signal = ${create}();`
 
-          const expectedOutput = `${importStatement}
-const signal = ${creator}(5, ${extraArg}{
-  name: "signal"
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
-
-        test('empty options', () => {
-          const src = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{});`
-
-          const expectedOutput = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{
-  name: "signal"
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
-
-        test('options excluding name', () => {
-          const src = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{ equals: false, ...rest });`
-
-          const expectedOutput = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{
-  name: "signal",
-  equals: false,
-  ...rest
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
-
-        test('options including name', () => {
-          const src = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{ equals: false, name: "foo", ...rest });`
-
-          const expectedOutput = `${importStatement}
-const rest = {};
-const signal = ${creator}(5, ${extraArg}{
-  equals: false,
-  name: "foo",
-  ...rest
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
-
-        test('array of length 1', () => {
-          const src = `${importStatement}
-const [signal] = ${creator}(5);`
-
-          const expectedOutput = `${importStatement}
-const [signal] = ${creator}(5, ${extraArg}{
-  name: "signal"
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
-
-        test('array of length 2', () => {
-          const src = `${importStatement}
-const [signal, setSignal] = ${creator}(5);`
-
-          const expectedOutput = `${importStatement}
-const [signal, setSignal] = ${creator}(5, ${extraArg}{
-  name: "signal"
-});`
-
-          assertTransform(src, expectedOutput, plugin)
-        })
+        assertTransform(src, src, plugin, true)
       })
-    }
-  })
-}
 
-// Negative tests
-for (const [create, module] of [
-  ['createSignal', 'solid-js/store'],
-  ['createMemo', 'solid-js/store'],
-  ['createStore', 'solid-js'],
-  ['createMutable', 'solid-js'],
-] as const) {
-  describe(create, () => {
-    test(`no import`, () => {
-      const src = `const signal = ${create}();`
+      test(`incorrect import`, () => {
+        const src = `import { ${create} } from "${module}";
+  const signal = ${create}();`
 
-      assertTransform(src, src, plugin)
+        assertTransform(src, src, plugin, true)
+      })
     })
+  }
+})
 
-    test(`incorrect import`, () => {
-      const src = `import { ${create} } from "${module}";
-const signal = ${create}();`
+describe('effect primitives', () => {
+  const fnName = 'fn'
 
-      assertTransform(src, src, plugin)
+  // Positive tests
+  for (const create of ['createEffect', 'createRenderEffect', 'createComputed'] as const) {
+    describe(create, () => {
+      for (const [type, importStatement, creator] of [
+        ['named import', `import { ${create} } from "solid-js";`, create],
+        ['renamed import', `import { ${create} as foo } from "solid-js";`, 'foo'],
+        ['namespace import', `import * as foo from "solid-js";`, `foo.${create}`],
+      ] as const) {
+        describe(type, () => {
+          testTransform(
+            'in a function declaration',
+            `${importStatement}
+            function ${fnName}() {
+              ${creator}(() => {});
+            }`,
+            `${importStatement}
+            function ${fnName}() {
+              ${creator}(() => {}, undefined, {
+                name: "in_${fnName}"
+              });
+            }`,
+            plugin,
+            true,
+          )
+        })
+
+        testTransform(
+          'in an arrow function',
+          `${importStatement}
+          const ${fnName} = () => {
+            ${creator}(() => {});
+          };`,
+          `${importStatement}
+          const ${fnName} = () => {
+            ${creator}(() => {}, undefined, {
+              name: "in_${fnName}"
+            });
+          };`,
+          plugin,
+          true,
+        )
+
+        testTransform(
+          'in a function expression',
+          `${importStatement}
+          const ${fnName} = function () {
+            ${creator}(() => {});
+          };`,
+          `${importStatement}
+          const ${fnName} = function () {
+            ${creator}(() => {}, undefined, {
+              name: "in_${fnName}"
+            });
+          };`,
+          plugin,
+          true,
+        )
+
+        testTransform(
+          'passed as a callback',
+          `${importStatement}
+            ${fnName}(() => {
+              ${creator}(() => {});
+            });`,
+          `${importStatement}
+            ${fnName}(() => {
+              ${creator}(() => {}, undefined, {
+                name: "to_${fnName}"
+              });
+            });`,
+          plugin,
+          true,
+        )
+      }
     })
-  })
-}
+  }
+
+  // Negative tests
+  for (const create of ['createEffect', 'createRenderEffect', 'createComputed'] as const) {
+    describe(create, () => {
+      test(`no import`, () => {
+        const src = `function ${fnName}() {
+          ${create}(() => {});
+        }`
+
+        assertTransform(src, src, plugin, true)
+      })
+    })
+  }
+})
