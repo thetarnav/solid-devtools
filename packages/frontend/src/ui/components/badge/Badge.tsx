cd0d95d11654bf9da32a744d7ba23a02bca7e644
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-25 22:00:24 +0200

    Move badge to unocss

--- packages/frontend/src/ui/components/badge/Badge.tsx
+++
@@ -1,6 +0,0 @@
-import { ParentComponent } from 'solid-js'
-import * as styles from './badge.css'
-
-export const Badge: ParentComponent = props => {
-    return <div class={styles.badge}>{props.children}</div>
-}
--- packages/frontend/src/ui/index.ts
+++ packages/frontend/src/ui/index.ts
@@ -1,16 +1,11 @@
 import './reset.css'
 
-export * from './components/badge/Badge'
+export * from './components/badge'
 export * from './components/button/Button'
 export * from './components/error-overlay/ErrorOverlay'
 export * from './components/highlight/Highlight'
 export { Skeleton } from './components/loading/Skeleton'
-export {
-    Node_Name,
-    Node_Type_Icon,
-    Owner_Name,
-    createHighlightedOwnerName,
-} from './components/owner-name'
+export * from './components/owner-name'
 export { Scrollable } from './components/scrollable/Scrollable'
 export * as Splitter from './components/splitter/Splitter'
 export { ToggleTabs } from './components/toggle-tabs/ToggleTabs'
