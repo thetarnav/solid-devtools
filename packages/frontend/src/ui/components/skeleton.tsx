4b137e4a68eb827d6488907a4974ec2bcb1eaaf4
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-26 19:27:26 +0200

    Move skeleton to unocss

--- packages/frontend/src/ui/components/skeleton.tsx
+++ packages/frontend/src/ui/components/skeleton.tsx
@@ -1,9 +1,12 @@
 import { createPolled } from '@solid-primitives/timer'
 import { Component } from 'solid-js'
-import * as styles from './Skeleton.css'
 
 export const Skeleton: Component<{}> = () => {
     const nDots = createPolled((p: number = 0) => (p === 3 ? 1 : ++p), 800)
 
-    return <div class={styles.skeleton}>Loading{Array.from({ length: nDots() }, () => '.')}</div>
+    return (
+        <div class="center-child w-full h-full bg-gray-200 text-gray-600">
+            Loading{Array.from({ length: nDots() }, () => '.')}
+        </div>
+    )
 }
