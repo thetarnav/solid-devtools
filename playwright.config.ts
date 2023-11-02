e0115b2f9b9904617dfac0a3f41caffdd30f09ca
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-02 09:37:21 +0100

    Correct CI check in e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
+const is_ci = !!process.env['CI']
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -8,11 +10,11 @@ export default defineConfig({
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env['CI'],
+    forbidOnly: is_ci,
     /* Retry on CI only */
-    retries: process.env['CI'] ? 2 : 0,
+    retries: is_ci ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env['CI'] ? 1 : undefined,
+    workers: is_ci ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
